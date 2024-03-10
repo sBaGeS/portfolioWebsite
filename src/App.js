8889a70b1eb4757200329c88ea7c63dd75d1023a
sBaGeS <112703434+sBaGeS@users.noreply.github.com>
2024-03-10 23:14:24 +0200

    website update

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
     <BrowserRouter>
    <Navbar/>
    <Routes>
-    <Route path="/" element={<Home />}></Route>
-    <Route path="/all_projects/" element={<ProjectTemplate />}></Route>
-    <Route path="/profile/" element={<Profile />}></Route>
-    <Route path="/project1/" element={<Project1 />}></Route>
-    <Route path="/project2/" element={<Project2 />}></Route>
-    <Route path="/project3/" element={<Project3 />}></Route>
-    <Route path="/project4/" element={<Project4 />}></Route>
+    <Route path="/portfolioWebsite/" element={<Home />}></Route>
+    <Route path="/portfolioWebsite/all_projects" element={<ProjectTemplate />}></Route>
+    <Route path="/portfolioWebsite/profile/" element={<Profile />}></Route>
+    <Route path="/portfolioWebsite/project1" element={<Project1 />}></Route>
+    <Route path="/portfolioWebsite/project2" element={<Project2 />}></Route>
+    <Route path="/portfolioWebsite/project3" element={<Project3 />}></Route>
+    <Route path="/portfolioWebsite/project4" element={<Project4 />}></Route>
    </Routes>
    <Footer/>
    </BrowserRouter>
